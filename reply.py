0aec779135acad0460239735e40a5c34c318efc4
harrynguon <harrynguon@gmail.com>
2017-06-28 22:50:18 +1200

    Update reply.py

--- reply.py
+++ reply.py
@@ -1,23 +1,24 @@
 #forms the automated reply for the grammar comments
 def make_reply_grammar(comment):
     print("Match found! Comment ID: " + comment.id)
-    comment.reply("Should *have*, could *have*, would *have*.")
+    comment.reply("Should *have*, could *have*, would *have*"
+	".\n\n*I am a bot. Please"
+	" don't hesitate to PM me any questions.*")
     print("Reply successful!")
     #cache.append(comment.id+',')
     comment_text_file = open("commentid.txt", 'a')
     comment_text_file.write(comment.id+',')
     comment_text_file.close()
-    cache.append(comment.id)
-	
 	
 #creates the automated reply to the sadfaced comments
 def make_reply_sadface(comment):
     print("Match found! Comment ID: " + comment.id)
     comment.reply("I saw that you typed a sad face emoticon in"
     " your comment, so I just wanted to let you "
-    "know that I hope you have a wonderful day!")
+    "know that I hope you have a wonderful day!"
+	"\n\n *I am a bot. Please don't hesitate to"
+	" PM me any questions.*")
     print("Reply successful!")
     sadface_text_file = open("sadcommentid.txt", 'a')
     sadface_text_file.write(comment.id+',')
     sadface_text_file.close()
-    sad_face_cache.append(comment.id)
